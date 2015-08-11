f6921553c4d344f94eb53a72d609806faf6e5523
Ryan <none@none>
2015-08-11 21:30:35 +0200

    Normalized spaces.

--- publicize-with-hashtags.php
+++ publicize-with-hashtags.php
@@ -49,31 +49,31 @@ function publicize_with_hashtags() {
     // If you do and you feel like you should, please contact me so I'll update the plugin aswell.
     $meta = '_wpas_mess';
     
-	$p = get_post();
-	if (empty($p)) return;
+    $p = get_post();
+    if (empty($p)) return;
     // Get the current post social message
-	$mess = get_post_meta($p->ID, $meta, true);
+    $mess = get_post_meta($p->ID, $meta, true);
     // Performance-wise length check: do nothing if we're already out of threshold.
     if (!empty($mess) && !empty($mess_max_length) && $mess_max_length <= (strlen($mess))) return;
-	// Get the post tags array
-	$ta = get_the_tags($p->ID);	
+    // Get the post tags array
+    $ta = get_the_tags($p->ID);	
     // Performance-wise tag check: do nothing if we don't (yet) have tags to process.
-	if (empty($ta)) return;
+    if (empty($ta)) return;
 
     // From now on, we mean business. Let's start by initializing the social message (if needed).
     if (empty($mess)) $mess = '';
-	// Create list of tags with hashtags in front of them until threshold is reached
-	foreach ($ta as $t) {
+    // Create list of tags with hashtags in front of them until threshold is reached
+    foreach ($ta as $t) {
         // Create the hashtag, stripping spaces if needed.
-		$ht = '#' . (($strip_spaces) ? str_replace(' ', '', $t->name) : $t->name);
-		// only process newly-added hashtags, skipping duplicate ones
-		if (stripos($mess,$ht) === false) {	
-			if (!empty($mess_max_length) && $mess_max_length <= (strlen($mess) + strlen($ht))) break;
+        $ht = '#' . (($strip_spaces) ? str_replace(' ', '', $t->name) : $t->name);
+        // only process newly-added hashtags, skipping duplicate ones
+        if (stripos($mess,$ht) === false) {	
+            if (!empty($mess_max_length) && $mess_max_length <= (strlen($mess) + strlen($ht))) break;
             $mess .= ' '.$ht;
-		}
-	}
-	// Update the new social message
-	update_post_meta($p->ID, $meta, $mess);
+        }
+    }
+    // Update the new social message
+    update_post_meta($p->ID, $meta, $mess);
 }
 
 // Hook the plugin action to 'save_post' handler.
