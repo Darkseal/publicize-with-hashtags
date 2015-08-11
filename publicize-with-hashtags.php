bb45d0ef6a78b1ae8ebf347b96f7d57505e9a100
Neilgaiman@b8457f37-d9ea-0310-8a92-e5e31aec5664 <Neilgaiman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 01:32:24 +0000

    Initial commit.

---
+++ publicize-with-hashtags.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Publicize With Hashtags
+Description: Automatically append hashtags to any content sent by Jetpack's Publicize module: hashtags will be created using post tags: dupe check and an optional length-based threshold are also included.
+Version: 0.1
+License: GPL
+Author: Darkseal
+Author URI: http://www.ryadel.com/
+ */
+
+// If Jetpack is not installed or Publicize module isn't active, just exit now.
+if (!class_exists('Jetpack') || !Jetpack::is_module_active('publicize')) return;
+
+function publicize_with_hashtags() {
+    
+    // Set an optional character threshold (set to null to disable).
+    // If you set it as a positive integer, hashtags will be appended only until this threshold is reached. 
+    // Default is 118, which is good for Twitter considering a default, non-https url length, which is 22 chars as stated by Twitter standards.
+    // For more info, see https://dev.twitter.com/overview/api/counting-characters and https://dev.twitter.com/overview/t.co Twitter official docs.
+    // Suggested values: 
+    // 118      if you plan to use only HTTP URLs
+    // 117      if you plan to use both HTTP and HTTPS (or only HTTPS) URLs
+    // 140      if you don't plan to append URL infos to your posts
+    // 0        or <null> or <false>, if you want to tufn-off this check and append hashtags without any char limits - good if you're only using Facebook sharing.
+    $mess_max_length = 118;
+    
+    // Set it to TRUE to strip all spaces when converting Tags to Hashtags, i.e. 'Windows 8' -> '#Windows8', FALSE to keep only first.
+    // Default is TRUE (strip all spaces).
+    $strip_spaces = true;
+    
+    // meta tag used by Publicize to store social message. Change this only if you're really know what you're doing.
+    // If you do and you feel like you should, please contact me so I'll update the plugin aswell.
+    $meta = '_wpas_mess';
+    
+	$p = get_post();
+	if (empty($p)) return;
+    // Get the current post social message
+	$mess = get_post_meta($p->ID, $meta, true);
+    // Performance-wise length check: do nothing if we're already out of threshold.
+    if (!empty($mess) && !empty($mess_max_length) && $mess_max_length <= (strlen($mess))) return;
+	// Get the post tags array
+	$ta = get_the_tags($p->ID);	
+    // Performance-wise tag check: do nothing if we don't (yet) have tags to process.
+	if (empty($ta)) return;
+
+    // From now on, we mean business. Let's start by initializing the social message (if needed).
+    if (empty($mess)) $mess = '';
+	// Create list of tags with hashtags in front of them until threshold is reached
+	foreach ($ta as $t) {
+        // Create the hashtag, stripping spaces if needed.
+		$ht = '#' . ($strip_spaces) ? str_replace(' ', '', $t->name) : $t->name;
+		// only process newly-added hashtags, skipping duplicate ones
+		if (stripos($mess,$ht) === false) {	
+			if (!empty($mess_max_length) && $mess_max_length <= (strlen($mess) + strlen($ht))) break;
+            $mess .= ' '.$ht;
+		}
+	}
+	// Update the new social message
+	update_post_meta($p->ID, $meta, $mess);
+}
+
+// Hook the plugin action to 'save_post' handler.
+add_action('save_post', 'publicize_with_hashtags', 99);
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+﻿=== Plugin Name ===
+Contributors: neilgaiman
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JSTHA4HMVSQ8J
+Tags: jetpack, publicize, hashtag, hashtags
+Requires at least: 3.0.1
+Tested up to: 4.2.4
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically append hashtags to content sent by Jetpack Publicize module. Hashtags will be created using post tags. Dupe and length check included.
+
+== Description ==
+
+**IMPORTANT**: This plugin requires Jetpack's Publicize module: read below for additional info.
+
+As the name says, this plugin will automatically append hashtags to any content sent by **Jetpack's Publicize** module. This means that **the plugin will work only if the Jetpack plugin is already installed on your Wordpress platform with the Publicize module set to active**. Don't worry, tho: if any of these requirements are missing this plugin won't do anything, without breaking your blog or compromising its performance in any way.
+
+**Characteristics**
+
+* Hashtags will be created using the post `tags`, (optionally) trimming the space chars, until the maximum character length is reached (if any).
+* You'll still be able to manually add custom hashtags, pre-pending or appending them to the auto-generated ones just like you always did.
+* You can reorder the hashtags as you like, regardless of how they were added.
+* Dupe check included, ensuring that the same hashtag won't be included multiple times - unless you manually do that.
+* Automatically added hashtags won't break the *118-character limits* of the social message text to keep the message length under the standard *tweet + link* limit.
+* The above character-length threshold can be changed and/or set to infinite with ease.
+
+== Installation ==
+
+1. Upload the **publicize-with-hashtags.zip** file using the Wordpress *upload plugin* feature OR unpack its contents inside the `/wp-content/plugins/` folder.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Take a look at the plugin's source code and change its default settings if needed.
+
+== Frequently Asked Questions ==
+
+None yet.
+
+== Screenshots ==
+
+None yet.
+
+== Changelog ==
+
+= 0.1 =
+* First release.
+
+== Upgrade Notice ==
+
+= 0.1 =
+* First release.
+
+== Authors ==
+
+* **Darkseal** of [Ryadel.com](http://www.ryadel.com/).
+
+<?php code(); ?>
